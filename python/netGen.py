c59a4eee07cdafdd74664e347e9309f4e2f92e3d
Ellsworth Campbell <ells@psu.edu>
2014-06-21 19:22:12 -0600

    check bank solvency function

--- python/netGen.py
+++ python/netGen.py
@@ -17,8 +17,12 @@ def generateNetwork(numberOfNodes, powerLawAlpha):
     ## loop through all nodes and set capacity equal to degree
     for i in range(0,len(Graph.node)-1):
         Graph.node[i]['capacity'] = Graph.degree(i)
+        ## right now capacity = degree
         Graph.node[i]['cumulativeShock'] = 0
+        ## if a bank is solvent, status = 1. When it crashes, status = 0.
         Graph.node[i]['status'] = 1
+        ## here we set the timestep that the bank becomes insolvent to a big number
+        Graph.node[i]['insolventTimestep'] = 100000000
    
     return Graph
   
@@ -37,15 +41,31 @@ def generateConnectedPowerLawNetwork(numberOfNodes, powerLawAlpha):
 def calculateDegreeAssortativity(Graph):
     return nx.degree_assortativity_coefficient(Graph)
     
+def checkSolvency(Graph, timestep):
+    for nodeID in range(0, numberOfNodes-1):
+        capacity = Graph.node[nodeID]['capacity']
+        cumulativeShock = Graph.node[nodeID]['cumulativeShock']
+        solventNeighbors = 0;
+        neighbors = Graph.neighbors(nodeID)
+        for neighborID in range(0, len(neighbors)):
+            neighbor = Graph.node[neighborID]
+            if neighbor['status'] == 1: solventNeighbors = solventNeighbors + 1
+            
+        if capacity < (cumulativeShock / solventNeighbors): 
+            Graph.node[nodeID]['status'] = 0
+            Graph.node[nodeID]['insolventTimestep'] = timestep
+        
+
+        
+        
+        
+     
+            
+        
+        
     
 numberOfNodes = 250
 powerLawAlpha = 2
 Graph = generateConnectedPowerLawNetwork(numberOfNodes, powerLawAlpha)
 degreeAssortativity = calculateDegreeAssortativity(Graph)
 print degreeAssortativity
-
-
-
-
-
-
